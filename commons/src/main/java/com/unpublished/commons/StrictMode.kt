db39c71805eb429fab078198a87b21905fe98510
Unpublished <unpublished@gmx.net>
2023-02-18 12:56:26 +0100

    Add StrictMode

---
+++ commons/src/main/java/com/unpublished/commons/StrictMode.kt
@@ -0,0 +1,52 @@
+package com.unpublished.commons
+
+import android.os.Build
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
+
+@JvmOverloads
+fun setupStrictMode(
+    permitActivityLeaks: Boolean = false,
+    permitNonSdkApiUsage: Boolean = false
+) {
+    StrictMode.enableDefaults()
+    val threadPolicyBuilder = ThreadPolicy.Builder(StrictMode.getThreadPolicy())
+    val badManufactures = hashSetOf("Nubia", "Xiaomi").map { m -> m.lowercase() }
+    val isBad = Build.MANUFACTURER.lowercase() in badManufactures
+    if (!isBad) {
+        threadPolicyBuilder.penaltyDeath()
+    }
+    StrictMode.setThreadPolicy(threadPolicyBuilder.build())
+    val builder = VmPolicy.Builder().penaltyLog()
+        .detectLeakedSqlLiteObjects()
+        .detectLeakedClosableObjects()
+        .detectLeakedRegistrationObjects()
+        .detectFileUriExposure()
+
+    if (!permitActivityLeaks) {
+        builder.detectActivityLeaks()
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        builder.detectCleartextNetwork()
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        builder.detectContentUriWithoutPermission()
+        builder.detectUntaggedSockets()
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && !permitNonSdkApiUsage) {
+        builder.detectNonSdkApiUsage()
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        builder.detectCredentialProtectedWhileLocked()
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        builder.detectIncorrectContextUse()
+        builder.detectUnsafeIntentLaunch()
+    }
+    if (!isBad) {
+        builder.penaltyDeath()
+    }
+    StrictMode.setVmPolicy(builder.build())
+}
